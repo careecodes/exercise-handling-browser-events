1b4568e4017328132cbe6ee97437ee47bb96c90d
Caree Youngman <caree@caree.codes>
2021-11-06 11:49:15 -0500

    got country to work

--- 2-medium/madlibs.js
+++ 2-medium/madlibs.js
@@ -1,15 +1,26 @@
 // Make a variable for the animal
 const animalSpan = document.querySelector(".animal");
 const animalInput = document.querySelector("#animal");
+
+// Country
+const countrySpan = document.querySelector(".country");
+
 const form = document.getElementById("madLibForm");
 let story = document.getElementById("madLibStory");
 
-// EventListener listens for click
+
+// Submit form
 form.addEventListener("submit",function(event){
+    let countryInput = document.getElementById("country").value;
+    console.log(countryInput);
+
     event.preventDefault();
     
     // Set inner text of span to input value
     animalSpan.innerText = animalInput.value;
+
+    // Set inner text of country to the input value
+    countrySpan.innerText = countryInput;
 
     // display the whole story
     story.style.display = "block";
