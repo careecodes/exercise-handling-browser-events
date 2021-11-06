72d40c54a537fa3ec76c91f0c863312c19710811
Caree Youngman <caree@caree.codes>
2021-11-06 11:56:11 -0500

    device field is working

--- 2-medium/madlibs.js
+++ 2-medium/madlibs.js
@@ -3,6 +3,7 @@ const animalSpan = document.querySelector(".animal");
 const countrySpan = document.querySelector(".country");
 const pluralNounSpan = document.querySelector(".pluralNoun");
 const foodSpan = document.querySelector(".food");
+const deviceSpan = document.querySelector(".device");
 
 const form = document.getElementById("madLibForm");
 const story = document.getElementById("madLibStory");
@@ -17,12 +18,14 @@ form.addEventListener("submit",function(event){
     let countryInput = document.querySelector("#country").value;
     let pluralNounInput = document.querySelector("#pluralNoun").value;
     let foodInput = document.querySelector("#food").value;
+    let deviceInput = document.querySelector("#device").value;
     
     // Set inner text of span to input value
     animalSpan.innerText = animalInput;
     countrySpan.innerText = countryInput;
     pluralNounSpan.innerText = pluralNounInput;
     foodSpan.innerText = foodInput;
+    deviceSpan.innerText = deviceInput;
 
     // display the whole story
     story.style.display = "block";
